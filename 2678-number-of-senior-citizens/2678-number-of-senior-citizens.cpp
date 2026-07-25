4d41b4ebcb5390fb4357cd502e03cb946de4b1a6
Japinder Singh <97287662+Jsingh26@users.noreply.github.com>
2026-07-26 00:03:28 +0530

    Time: 0 ms (100%), Space: 17.2 MB (38%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for(string s : details){
+            if(s[11]> '6'){
+                count ++;
+            }
+            else if(s[11]=='6' && s[12]!='0'){
+                count ++;
+            }
+        }
+        return count;
+    }
+};
